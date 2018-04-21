3eb5578b2d78a0f0be2d7d033f855c28774b53c7
Dominicinator <dominicdonahue@gmail.com>
2018-04-21 22:01:45 +1200

    Player Basic01
    Moving player object to mouse position

---
+++ AstroMage/Assets/Player/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public Trajectory trajectory;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        trajectory.startPos = transform.position;
+        Vector3 temp_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        temp_pos.z = 0f;
+        trajectory.endPos = temp_pos;
+        transform.position = trajectory.endPos;
+	}
+}
---
+++ AstroMage/Assets/Player/Trajectory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trajectory : MonoBehaviour
+{
+    public Vector2 startPos;
+    public Vector2 endPos;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+
+
+}
